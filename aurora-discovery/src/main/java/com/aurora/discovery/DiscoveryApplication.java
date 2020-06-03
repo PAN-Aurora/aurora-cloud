5baf2af64e89c63416e9b17432321f7e17b3c001
panhuaqing <panhuaqing@bluetop.com.cn>
2020-06-03 14:03:16 +0800

    初始化项目

---
+++ aurora-discovery/src/main/java/com/aurora/discovery/DiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.aurora.discovery;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册中心
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+}
